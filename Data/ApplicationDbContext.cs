f3471c75552636bac79a61f920a05f36f58a53e5
Lukasz <lukasz.bulicz96@gmail.com>
2023-12-10 16:02:05 +0100

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.Nickname)
+                .IsRequired(false)
+                .HasMaxLength(32);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.FirstName)
+                .IsRequired(false)
+                .HasMaxLength(32);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .Property(e => e.LastName)
+                .IsRequired(false)
+                .HasMaxLength(32);
+        }
+    }
+}
